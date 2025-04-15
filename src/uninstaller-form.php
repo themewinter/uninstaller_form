00b06ac6da53ddf27ac056d4f596a4f2962b3eef
emaratHossain <badhon@arraytics.com>
2025-04-15 11:50:46 +0600

    Update: dynamically handle script-handler

--- src/uninstaller-form.php
+++ src/uninstaller-form.php
@@ -3,7 +3,7 @@
 namespace UninstallerForm;
 
 class UninstallerForm {
-    public static function init($plugin_name, $plugin_slug, $plugin_file) {
-        (new HookRegistrar($plugin_name, $plugin_slug, $plugin_file))->register();
+    public static function init($plugin_name, $plugin_slug, $plugin_file, $script_handler) {
+        (new HookRegistrar($plugin_name, $plugin_slug, $plugin_file,$script_handler))->register();
     }
 }
