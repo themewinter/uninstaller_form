c26981be82da17568768faffd7b82c72b7573371
emaratHossain <badhon@arraytics.com>
2025-04-17 15:48:18 +0600

    Update:  localization

--- src/Support/Localizer.php
+++ src/Support/Localizer.php
@@ -34,7 +34,7 @@ class Localizer {
      * @since 1.0.0
      */
     public function handle() {
-        wp_localize_script($this->script_handle, 'UninstallerData', [
+        wp_localize_script($this->script_handle, 'feedback', [
             'restUrl'    => rest_url('uninstaller-form/v1/feedback'),
             'nonce'      => wp_create_nonce('wp_feedback'),
             'pluginName' => $this->name,
