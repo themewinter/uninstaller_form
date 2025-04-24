c7a0b8e78be195d295f8830f954550f2e9432bdd
emaratHossain <badhon@arraytics.com>
2025-04-24 12:33:39 +0600

    Update: data storing process to sheet updated

--- src/Support/GoogleSheetClient.php
+++ src/Support/GoogleSheetClient.php
@@ -1,9 +1,6 @@
 <?php
 namespace UninstallerForm\Support;
 
-use Google_Client;
-use Google_Service_Sheets;
-
 /**
  * GoogleSheetClient class for the uninstaller form.
  * 
@@ -12,100 +9,111 @@ use Google_Service_Sheets;
  * @package UNINSTALLER_FORM
  */
 class GoogleSheetClient {
-    protected $client;
-    protected $service;
+    protected $credentials;
     protected $spreadsheetId;
     protected $sheetName;
+    protected $accessToken;
 
     /**
      * GoogleSheetClient Constructor.
      * 
-     * @param string $credentialsPath The path to the Google credentials file.
-     * @param string $spreadsheetId The ID of the Google spreadsheet.
-     * @param string $sheetName The name of the Google sheet.
+     * @param string $credentialsPath The path to the credentials file.
+     * @param string $spreadsheetId The ID of the Google Sheet.
+     * @param string $sheetName The name of the sheet to write to. Defaults to 'Sheet1'.
      * 
      * @since 1.0.0
-     */
+     */ 
     public function __construct($credentialsPath, $spreadsheetId, $sheetName = 'Sheet1') {
+        $this->credentials   = json_decode(file_get_contents($credentialsPath), true);
         $this->spreadsheetId = $spreadsheetId;
         $this->sheetName     = $sheetName;
-
-        $this->client = new Google_Client();
-        $this->client->setAuthConfig($credentialsPath);
-        $this->client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
-        $this->service = new Google_Service_Sheets($this->client);
+        $this->accessToken   = $this->generateAccessToken();
     }
 
     /**
-     * Get data from a specific row in the Google sheet.
-     * 
-     * @param array $values The row number to retrieve data from.
-     * 
-     * @return array The data from the specified row.
+     * Generate an access token for the Google Sheets API.
      * 
      * @since 1.0.0
      */
-    public function appendRow(array $values) {
-        $sheets = $this->service->spreadsheets->get($this->spreadsheetId)->getSheets();
-        $sheetExists = false;
+    protected function generateAccessToken() {
+        $header = [
+            "alg" => "RS256",
+            "typ" => "JWT"
+        ];
 
-        foreach ($sheets as $sheet) {
-            if ($sheet->getProperties()->getTitle() === $this->sheetName) {
-                $sheetExists = true;
-                break;
-            }
-        }
+        $now = time();
+        $claimSet = [
+            "iss"   => $this->credentials['client_email'],
+            "scope" => "https://www.googleapis.com/auth/spreadsheets",
+            "aud"   => "https://oauth2.googleapis.com/token",
+            "iat"   => $now,
+            "exp"   => $now + 3600,
+        ];
 
-        // Create sheet and add column headers if it doesn't exist
-        if (!$sheetExists) {
-            $addSheetRequest = new \Google_Service_Sheets_Request([
-                'addSheet' => [
-                    'properties' => [
-                        'title' => $this->sheetName,
-                    ],
-                ],
-            ]);
+        $jwtHeader = $this->base64UrlEncode(json_encode($header));
+        $jwtClaims = $this->base64UrlEncode(json_encode($claimSet));
+        $signatureInput = "$jwtHeader.$jwtClaims";
 
-            $batchUpdateRequest = new \Google_Service_Sheets_BatchUpdateSpreadsheetRequest([
-                'requests' => [$addSheetRequest],
-            ]);
+        // Sign the JWT
+        openssl_sign($signatureInput, $signature, $this->credentials['private_key'], 'sha256WithRSAEncryption');
+        $jwtSignature = $this->base64UrlEncode($signature);
+        $jwt = "$signatureInput.$jwtSignature";
 
-            $this->service->spreadsheets->batchUpdate($this->spreadsheetId, $batchUpdateRequest);
-
-            // Add column headers after creating the new sheet
-            $headers = [
-                'Customer Name',
-                'Customer Email',
-                'Plugin Name',
-                'Reason',
-                'Feedback',
-                'Timestamp'
-            ];
-
-            $headerBody = new \Google_Service_Sheets_ValueRange([
-                'values' => [$headers],
-            ]);
-
-            $this->service->spreadsheets_values->append(
-                $this->spreadsheetId,
-                $this->sheetName,
-                $headerBody,
-                ['valueInputOption' => 'RAW']
-            );
-        }
-
-        // Append the actual data
-        $body = new \Google_Service_Sheets_ValueRange([
-            'values' => [$values],
+        // Exchange JWT for access token
+        $response = wp_remote_post('https://oauth2.googleapis.com/token', [
+            'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+            'body' => [
+                'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                'assertion' => $jwt,
+            ],
         ]);
-        $params = ['valueInputOption' => 'RAW'];
 
-        return $this->service->spreadsheets_values->append(
-            $this->spreadsheetId,
-            $this->sheetName,
-            $body,
-            $params
-        );
+        $body = json_decode(wp_remote_retrieve_body($response), true);
+
+        return $body['access_token'] ?? null;
     }
 
+    /**
+     * Base64 URL encode a string.
+     * 
+     * @since 1.0.0
+     * 
+     * @param string $data The string to encode.
+     * 
+     * @return string The base64 URL encoded string.
+     * */
+    protected function base64UrlEncode($data) {
+        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+    }
+
+    /**
+     * Append a row to the Google Sheet.
+     * 
+     * @since 1.0.0
+     * 
+     * @param array $values The values to append to the sheet.
+     * 
+     * @return array The response from the Google Sheets API.
+     * */
+    public function appendRow(array $values) {
+        $url = "https://sheets.googleapis.com/v4/spreadsheets/{$this->spreadsheetId}/values/{$this->sheetName}!A1:append?valueInputOption=RAW";
+
+        $body = [
+            'values' => [$values]
+        ];
+
+        $response = wp_remote_post($url, [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->accessToken,
+                'Content-Type'  => 'application/json',
+            ],
+            'body' => json_encode($body),
+        ]);
+
+        if (is_wp_error($response)) {
+            throw new \Exception('Google Sheets API request failed: ' . $response->get_error_message());
+        }
+
+        return json_decode(wp_remote_retrieve_body($response), true);
+    }
 }
